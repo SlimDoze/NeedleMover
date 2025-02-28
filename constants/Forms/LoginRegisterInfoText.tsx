0dd989705c6730996af461e98efba77f61cc4acc
SlimDoze <slim.doze.trap@gmail.com>
2025-03-01 00:36:44 +0100

    Revert "Merge pull request #2 from SlimDoze/feat-Welcome#2"
    This reverts commit d76198b13442d124f1e62c93ecb2e866d5bdac89, reversing
    changes made to 0f88641055c0716e69b608633e37fbc601e466d7.

--- constants/Forms/LoginRegisterInfoText.tsx
+++ constants/Forms/LoginRegisterInfoText.tsx
@@ -1,11 +1,11 @@
 export const Constant_FormInfoText = {
-    InputName: "Enter your Name",
-    InputHandle: "Enter your Handle",
-    InputEmail: "Enter your Email",
-    InputPassword: "Enter your Password",
-    PageTitle: "Neeedle Movers",
-    StayLoggedIn: "Stay logged in",
-    NeedleMover: "Needle Mover",
-    Continue: "Continue",
-    Register: "Register",
-  };
+  InputName: "Enter your Name",
+  InputHandle: "Enter your Handle",
+  InputEmail: "Enter your Email",
+  InputPassword: "Enter your Password",
+  PageTitle: "Neeedle Movers",
+  StayLoggedIn: "Stay logged in",
+  NeedleMover: "Needle Mover",
+  Continue: "Continue",
+  Register: "Register",
+};
