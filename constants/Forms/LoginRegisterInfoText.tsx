350739cdf2fe128781aa4d634b44f23fc09c5ac2
SlimDoze <slim.doze.trap@gmail.com>
2025-02-28 19:26:13 +0100

    test

---
+++ constants/Forms/LoginRegisterInfoText.tsx
@@ -0,0 +1,6 @@
+export const Constant_FormInfoText = {
+    InputName: "Enter your Name",
+    InputHandle: "Enter your Handle",
+    InputEmail: "Enter your Email",
+    InputPassword: "Enter your Password",
+  };
