f77aa3de0b9925f48a1d8f0e55f45f36ee04a849
SlimDoze <slim.doze.trap@gmail.com>
2025-03-13 15:26:18 +0100

    added comments to edgeFunction

--- supabase/functions/confirm-signup/index.ts
+++ supabase/functions/confirm-signup/index.ts
@@ -1,21 +1,37 @@
 import { serve } from 'https://deno.land/std@0.131.0/http/server.ts';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
+/* +++++++++++++++++++++++++++++++++++++++++++
+    !!! Code wird in der Cloud ausgeführt, kommuniziert wird hier Richtung app!!!
+
+      CORS = Cross-Origin Ressource Sharing
+      Erlaubt WebAnwendung auf externe Ressourcen zuzugreigen
+        > (Mail => EdgeFunction =>DB Confirm => Deeplink)
+  ++++++++++++++++++++++++++++++++++++++++++++*/
 const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Origin': '*', // Erlaubt Anfragen von allen Ursprüngen (Anfrage soll von verschiedenen Domains aus ausgerufen werden)
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type', // Gibt die Erlaubten Header in der Anfrage an
 };
 
 serve(async (req) => {
+  // [CORS] OPTIONS HTTP Request => Ensures external Servers Sequirity Guidelines 
+  // [CORS] OPTIONS HTTP Request => Ensures  CORS REQ / Methods / Header  Formats are accepted
   if (req.method === 'OPTIONS') {
+
+  // [CORS] OPTIONS HTTP Response => Header with information about the valid Headers is returned
     return new Response('ok', { headers: corsHeaders });
   }
 
+  // [CORS] Main Request
   try {
+    // Extracting Information out of edgeFunction URL
+    // https://your-edge-function-url?token=someToken&type=signup
+    
     const url = new URL(req.url);
     const token = url.searchParams.get('token');
     const type = url.searchParams.get('type');
 
+      // [Validates] Request Type and Token Exist
     if (!token || type !== 'signup') {
       return new Response(JSON.stringify({ error: 'Invalid token or type' }), {
         headers: { ...corsHeaders, 'Content-Type': 'application/json' },
@@ -23,6 +39,7 @@ serve(async (req) => {
       });
     }
 
+    // [Supabase] Getting environment Variables
     const supabaseUrl = Deno.env.get('SUPABASE_URL') as string;
     const supabaseServiceKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') as string;
     const supabase = createClient(supabaseUrl, supabaseServiceKey);
